c814ae321a38ce47f0fa03acc68b4dd3fbb45ea3
YoustinaRemon <tenaremon77@gmail.com>
2026-08-09 13:09:49 +0300

    Initalize app

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color mainColor = Color(0xffFAF6F0);
+  static const Color secondMainColor = Color(0xff4A7C59);
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:foodie_hup/foodie_hup.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+
